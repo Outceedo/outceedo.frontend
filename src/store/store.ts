38de27cb7092f36d42a8bf19025f1e5431ed766c
nikitha7688 <nikitha7688@gmail.com>
2025-04-15 20:16:23 +0530

    ui update

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
-import profileReducer from "./profileSlice";
+import profileReducer from "../store/profile-slice";
 
 const store = configureStore({
   reducer: {
