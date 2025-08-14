1aa04d1a1ef8172008e4177a2eee631f4be8ef33
relladinesh <relladinesh19@gmail.com>
2025-08-14 22:10:41 +0530

    teamup

--- src/Playerpages/SponsorApplications.tsx
+++ src/Playerpages/SponsorApplications.tsx
@@ -141,7 +141,7 @@ const SponsorApplicationpage = () => {
         <ArrowLeft className="w-5 h-5 mr-1" />
       </button>
       <div className="flex gap-4 mb-4">
-        <div className="relative w-1/3">
+        <div className="relative ">
           <FontAwesomeIcon
             icon={faSearch}
             className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
@@ -166,7 +166,7 @@ const SponsorApplicationpage = () => {
           </SelectContent>
         </Select>
       </div>
-      <div className="dark:bg-gray-800">
+      <div className="dark:bg-gray-800 w-xs md:w-full">
         <Table>
           <TableHeader>
             <TableRow>
